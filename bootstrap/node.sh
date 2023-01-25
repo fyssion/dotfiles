3aca6a63788719c1c9a4120604665f46c43bcf55
Fyssion <fyssioncodes@gmail.com>
2023-01-25 15:57:45 -0600

    Switch to n from nvm
    nvm causes significant slowdowns in my WSL2 environment

--- bootstrap/node.sh
+++ bootstrap/node.sh
@@ -1,13 +1,4 @@
-# Install nvm
+# Install n and node
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-# Load nvm
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Install node
-
-nvm install node
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
+npm install -g n
--- zshrc
+++ zshrc
@@ -34,7 +34,3 @@ path_prepend "$HOME/.local/bin"
 if [ -f ~/.zshrc_local ]; then
     source ~/.zshrc_local
 fi
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
