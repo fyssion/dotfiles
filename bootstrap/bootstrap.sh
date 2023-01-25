87051496cae07ce094208e07d9f782f91a19f605
Fyssion <fyssioncodes@gmail.com>
2023-01-25 16:02:03 -0600

    Add GitHub CLI installer

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -40,4 +40,16 @@ if prompt "Install PostgreSQL?"; then
     echo "Done!"
 fi
 
+# gh cli
+if prompt "Install GitHub CLI?"; then
+    # https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
+    type -p curl >/dev/null || sudo apt install curl -y
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+    echo "Done!"
+fi
+
 echo "Alright, looks like we're done here!"
