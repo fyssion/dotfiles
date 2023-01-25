ce0a194b5a4cad7dfab818de821ef6dad416f9ec
Fyssion <fyssioncodes@gmail.com>
2023-01-25 15:58:50 -0600

    Switch away from pyenv
    pyenv causes significant slowdowns in my WSL2 environment.
    in the future, I plan to use a workflow manager like pyflow or posey.
    for now, hatch running on a compiled python installation will do.
    I'd rather not touch the system python.

--- bootstrap/python.sh
+++ bootstrap/python.sh
@@ -1,24 +1,32 @@
-# Install pyenv
-
-curl https://pyenv.run | bash
-
-# Load pyenv
-
-export PYENV_ROOT="$HOME/.pyenv"
-path_prepend "$PYENV_ROOT/bin"
-eval "$(pyenv init --path)"
-eval "$(pyenv virtualenv-init -)"
-
 # Install Python deps
 
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-# Install Python
+# Install System Python
+PYTHON_VERSION=$1
 
-pyenv install $1
-pyenv global $1
+wget -O /tmp/Python-$PYTHON_VERSION.tar.xz https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
+tar -xf /tmp/Python-$PYTHON_VERSION.tar.xz -C /tmp
+pushd /tmp/Python-$PYTHON_VERSION
+
+echo "Installing Python $1 from source. This may take awhile..."
+./configure --enable-optimizations  # I won't be installing <3.6
+make
+sudo make install
+popd
+
+# Make default
+SHORT_VERSION=${PYTHON_VERSION%.*}
+[ -e "/usr/bin/python3" ] && sudo rm /usr/bin/python3
+sudo ln -s /usr/local/bin/python$SHORT_VERSION /usr/bin/python3
+[ -e "/usr/bin/python" ] && sudo rm /usr/bin/python
+sudo ln -s /usr/bin/python3 /usr/bin/python
+[ -e "/usr/bin/pip3" ] && sudo rm /usr/bin/pip3
+sudo ln -s /usr/local/bin/pip$SHORT_VERSION /usr/bin/pip3
+[ -e "/usr/bin/pip" ] && sudo rm /usr/bin/pip
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
 
 # Install pipx
 
